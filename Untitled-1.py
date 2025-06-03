ef0d186a44e5fe6a54d0c4f44932c8d88b00ba7a
Rodrigo Paione Grinfeld <ropgrinfeld@gmail.com>
2025-06-03 12:09:34 -0300

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,6 +1,23 @@
-import pandas as pd 
+import pandas as pd
 
-df1 = pd.read_csv("dataset_grupo1.csv")
+#importando os dados
+df1 = pd.read_csv("dataset_grupo1.csv", encoding="latin1", sep = ";")
 
-print(df1)
+df2 = pd.read_csv("dataset2_grupo1.csv", encoding="latin1", sep = ";")
 
+df3 = pd.read_csv("dataset3_grupo1.csv", encoding="latin1", sep = ";")
+
+#fazendo a junção dos dados em uma tabela única
+dados = pd.merge(df1, df2, on="Data")
+dados = pd.merge(dados, df3, on="Data")
+
+dados.head()
+ 
+ #conferindo os dados:
+print(dados.isnull().sum())
+#não há n.a values
+print(dados['Data'].value_counts().sort_index())
+# dados não repetidos
+print(dados.nunique())
+
+#aparentemente tudo certo :)
